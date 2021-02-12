0d56ac79479006203d761367cbe25ad4ca3b6926
pavliggs <51763058+pavliggs@users.noreply.github.com>
2021-02-13 01:25:24 +0300

    add @PreAuthorize

--- springsecuritydemo/src/main/java/com/khovaylo/springsecuritydemo/rest/SpringSecurityRestController.java
+++ springsecuritydemo/src/main/java/com/khovaylo/springsecuritydemo/rest/SpringSecurityRestController.java
@@ -1,6 +1,7 @@
 package com.khovaylo.springsecuritydemo.rest;
 
 import com.khovaylo.springsecuritydemo.model.Developer;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,11 +22,13 @@ public class SpringSecurityRestController {
     ).collect(Collectors.toList());
 
     @GetMapping
+    @PreAuthorize("hasAuthority('developer:read')") //instead antMatchers(...).hasAuthority(...) method from SpringConfig
     public List<Developer> getAll() {
         return DEVELOPERS;
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('developer:read')")
     public Developer getById(@PathVariable Long id) {
         return DEVELOPERS.stream().filter(developer -> developer.getId().equals(id))
                 .findFirst()
@@ -33,12 +36,14 @@ public class SpringSecurityRestController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('developer:write')")
     public Developer create(@RequestBody Developer developer) {
         DEVELOPERS.add(developer);
         return developer;
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('developer:write')")
     public void deleteById(@PathVariable Long id) {
         DEVELOPERS.removeIf(developer -> developer.getId().equals(id));
     }
--- springsecuritydemo/src/main/java/com/khovaylo/springsecuritydemo/security/SpringConfig.java
+++ springsecuritydemo/src/main/java/com/khovaylo/springsecuritydemo/security/SpringConfig.java
@@ -5,6 +5,7 @@ import com.khovaylo.springsecuritydemo.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,6 +20,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) //for @PreAuthorize from SpringSecurityRestController
 public class SpringConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -27,9 +29,6 @@ public class SpringConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/**").hasAuthority(Permission.DEVELOPER_READ.getPermission())
-                .antMatchers(HttpMethod.POST, "/api/**").hasAuthority(Permission.DEVELOPER_WRITE.getPermission())
-                .antMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(Permission.DEVELOPER_WRITE.getPermission())
                 .anyRequest()
                 .authenticated()
                 .and()
